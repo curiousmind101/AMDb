4b29c8ecc1ed922ae33fa486b738b044a09e4420
Ajit Kumar <101949618+curiousmind101@users.noreply.github.com>
2023-04-06 01:33:29 +0530

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img alt="header_img"  className="header__icon" src="AMDb.png" /></Link>
+                <Link to="/"><img alt="header_img"  className="header__icon" src="https://i.ibb.co/qRv61CY/AMDb.png" /></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
